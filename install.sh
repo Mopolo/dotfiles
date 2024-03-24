bb92b6e8c58e212ad3bc9f7ee4f03dca37e30645
Nathan Boiron <github@mopolo.net>
2024-03-24 15:23:13 +0100

    fix path

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf "$HOME"/.zshrc
-ln -s "$HOME"/.dotfiles/.zshrc "$HOME"/.zshrc
+ln -s "$HOME"/.dotfiles/shell/.zshrc "$HOME"/.zshrc
 
 # Removes .zprofile from $HOME (if it exists) and symlinks the .zprofile file from the .dotfiles
 rm -rf "$HOME"/.zprofile
